384bbd90fb8bf308aefc492f391ef3b9a3b02480
BaekJinH <tmdfyd6868@naver.com>
2022-11-11 13:53:54 +0900

    .

--- static/js/complet.js
+++ static/js/complet.js
@@ -84,3 +84,19 @@ function afterPrint()
 {
    document.body.innerHTML = initBody;
 }
+
+
+
+
+// 현재 날짜
+$(function(){
+
+let today = new Date();   
+
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+
+    document.getElementById('printText').innerHTML = year + '년 &nbsp;' + month + '월 &nbsp;' + date + '일 ' ;
+})
+
